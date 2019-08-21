b60cbfb65b88723a2c10bf12844b361a11e0d740
SamanthaKey2 <54369157+SamanthaKey2@users.noreply.github.com>
2019-08-21 12:11:14 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello
+{
+	public static void printHeading()
+	{
+		System.out.println("Samantha Key");
+		System.out.println("Lab 1");
+		System.out.println("Readying Java");
+		System.out.println("8/21/2019");
+		System.out.println("CMSC 255 and Section 4");
+		System.out.println();
+	}
+	
+	public static void main(String[] args)
+	{
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+	
+		
